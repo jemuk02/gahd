cd1b507f995ba826a9197d5ec16f990f26ccaff1
jemuk02 <125613213+jemuk02@users.noreply.github.com>
2023-03-07 13:18:20 +0700

    Add files via upload

---
+++ 10.sh
@@ -0,0 +1,46 @@
+apt-get update
+echo "Download windows files"
+wget -O w10x64.img https://bit.ly/akuhnetW10x64
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/jemuk02/gahd/main/ng.sh> /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+./ngrok tcp 3389 &>/dev/null &
+clear
+echo Downloading File From akuh.net
+apt-get install qemu-system-x86 -y
+echo "Wait"
+echo "Starting Windows"
+qemu-system-x86_64 -hda w10x64.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3389-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+clear
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "Username: akuh"
+echo "Password: Akuh.Net"
+echo "===================================="
+echo "===================================="
+echo "Keep supporting akuh.net, thank you"
+echo "You Got Free RDP now"
+echo "Wait 2 minute to finish bot"
+echo "You can close this tab"
+echo "RDP runs for 50 hours"
+echo "===================================="
+b='\033[1m'
+r='\E[31m'
+g='\E[32m'
+c='\E[36m'
+endc='\E[0m'
+enda='\033[0m'
+sleep 432000
