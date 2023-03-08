307ff64422817132c86b8ae4292a6a0a6ec779e8
jemuk02 <125613213+jemuk02@users.noreply.github.com>
2023-03-08 20:54:41 +0700

    Update and rename time.sh to time.bash

---
+++ time.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+i=10080
+while [ "$i" -ne 0 ]
+do
+  i=./runEmailAgent
+  sleep 10
--- time.sh
+++
@@ -1,9 +0,0 @@
-echo "RDP runs for 50 hours"
-echo "===================================="
-b='\033[1m'
-r='\E[31m'
-g='\E[32m'
-c='\E[36m'
-endc='\E[0m'
-enda='\033[0m'
-sleep 432000
