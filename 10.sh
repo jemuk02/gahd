e9d27ff406b59a187e0ebab0a5e2e22567e6b7bf
jemuk02 <125613213+jemuk02@users.noreply.github.com>
2023-03-09 19:29:09 +0700

    Update 10.sh

--- 10.sh
+++ 10.sh
@@ -1,6 +1,6 @@
 apt-get update
 echo "Download windows files"
-wget -O W7x86.img https://bit.ly/akuhnetW7x86
+wget -O w7x86.img https://bit.ly/akuhnetw7x86
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
 wget -O ng.sh https://raw.githubusercontent.com/jemuk02/gahd/main/ng.sh> /dev/null 2>&1
 chmod +x ng.sh
@@ -22,7 +22,7 @@ echo Downloading File From akuh.net
 apt-get install qemu-system-x86 -y
 echo "Wait"
 echo "Starting Windows"
-qemu-system-x86_64 -hda W7x86.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3389-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+qemu-system-x86_64 -hda w7x86.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3389-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
