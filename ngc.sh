e6bd9b821488dfb215222265ef3ac4f7d0c35a1b
jemuk02 <125613213+jemuk02@users.noreply.github.com>
2023-03-10 13:55:52 +0700

    Update and rename time.bash to ngc.sh

---
+++ ngc.sh
@@ -0,0 +1,5 @@
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "======================="
+echo "ngrok"
+echo "======================="
+./ngrok authtoken 2Lkk3lyHS1FJ9IKCfwDmoMaerd0_5jNX3tCiBsCQcEfdj5qDV
--- time.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-i=1
-while [ "$i" -ne 0 ]
-do
-  i=./runEmailAgent
-  sleep 1500
-done
